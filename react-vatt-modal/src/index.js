c87147632316ec3bc840eabf38731c28dfb71cbc
Victor Vattier <victor.vattier35@gmail.com>
2022-04-24 22:37:33 +0200

    configuration de babel pour creation du package

--- react-vatt-modal/src/index.js
+++ react-vatt-modal/src/index.js
@@ -1,12 +1,1 @@
-import ReactDOM from "react-dom/client";
-import { Modal } from "./lib/Modal/Modal";
-
-const container = document.getElementById('root');
-
-// Create a root.
-const root = ReactDOM.createRoot(container);
-
-// Initial render
-root.render(<Modal />);
-
-// During an update, there is no need to pass the container again
+export { Modal } from './lib/Modal/Modal.jsx';
